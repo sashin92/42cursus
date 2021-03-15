3bbf3e800d05920b01128ed3dc1d41257c1bf7b5
Sangbeom Shin <sashin@c6r7s6.42seoul.kr>
2021-03-15 14:22:40 +0900

    cub3d_test

--- cub3d/test.c
+++ cub3d/test.c
@@ -2,12 +2,13 @@
 
 int main(void)
 {
-    // void    *mlx_ptr;
+	void	*mlx_ptr;
+	void	*win_ptr;
 
-    // mlx_ptr = mlx_init();
+	mlx_ptr = mlx_init();
+	
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "sashin's window");
 
-    // mlx_new_window(mlx_ptr, 500, 500, "sashin's window");
-
-    // mlx_loop(mlx_ptr);
-    printf("hi");
+	mlx_loop(mlx_ptr);
+	printf("hi");
 }
