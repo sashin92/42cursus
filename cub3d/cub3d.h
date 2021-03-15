5370ae208c321c76c5e4ea7231b892dc4210de02
Sangbeom Shin <sashin@c6r7s6.42seoul.kr>
2021-03-15 13:58:22 +0900

    cub3d_test

---
+++ cub3d/cub3d.h
@@ -0,0 +1,16 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+
+# include "./mlx/mlx.h"
+
+// # include "libft.h"
+
+
+
+#endif
---
+++ cub3d/test.c
@@ -0,0 +1,13 @@
+#include "cub3d.h"
+
+int main(void)
+{
+    // void    *mlx_ptr;
+
+    // mlx_ptr = mlx_init();
+
+    // mlx_new_window(mlx_ptr, 500, 500, "sashin's window");
+
+    // mlx_loop(mlx_ptr);
+    printf("hi");
+}
