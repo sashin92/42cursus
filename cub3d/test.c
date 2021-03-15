8f09d432f55bb78bd29357f6052f1479d8f55db2
Sangbeom Shin <kill@Sangbeomui-MacBookAir.local>
2021-03-15 14:44:38 +0900

    cub3d_test

--- cub3d/test.c
+++ cub3d/test.c
@@ -4,10 +4,14 @@ int main(void)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+	void	*img_ptr;
+
+
 
 	mlx_ptr = mlx_init();
 	
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "sashin's window");
+	printf("Hello World");
 
 	mlx_loop(mlx_ptr);
 	printf("hi");
