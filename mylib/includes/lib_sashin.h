3f5629e2f410887ff5fe54d8fadcd1744005277a
Sangbeom Shin <sashin@c8r3s10.42seoul.kr>
2021-04-19 22:33:02 +0900

    organize my library(21/04/19)

---
+++ mylib/includes/lib_sashin.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lib_sashin.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sashin <sashin@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/19 15:02:52 by sashin            #+#    #+#             */
+/*   Updated: 2021/04/19 15:37:58 by sashin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIB_SASHIN_H
+# define LIB_SASHIN_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+# include "libft.h"
+
+# include "ft_printf.h"
+
+# include "get_next_line.h"
+
+#endif
