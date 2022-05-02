0e7b1769dd9518c144ad68d315c7aac91a9b5b9d
Sangbeom Shin <sashin@c2r4s8.42seoul.kr>
2022-05-02 20:18:00 +0900

    make new git, update nginx

---
+++ inception/backup_init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#self signed certification(for ssl)
+echo making rootCA...
+openssl req -new -newkey rsa:4096 -sha256 -nodes -keyout sashin-ssl.key \
+-x509 -days 90 -subj "/C=KR/ST=Seoul/L=podong_podong_Gaepo-dong/O=42seoul/OU=Cluster/CN=localhost" -out sashin-ssl.crt
+mv sashin-ssl.key /etc/ssl/private
+mv sashin-ssl.crt /etc/ssl/certs
+
+#service start
+service nginx start
+service php7.3-fpm start
+service mysql start
+service --status-all
+
+#set mysql
+mysql < set_mysql.sql
+mysqladmin -u root -p password
+service mysql reload
+
+bash
