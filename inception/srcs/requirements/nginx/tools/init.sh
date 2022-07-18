2de862ffe74466c96d4ddd1fcfa3311de46bf06f
sashin92 <aopaoi0987@naver.com>
2022-07-18 17:43:25 +0900

    fix env, nginx fastcgi

--- inception/srcs/requirements/nginx/tools/init.sh
+++ inception/srcs/requirements/nginx/tools/init.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# 
 echo making rootCA...
 openssl req -new -newkey rsa:4096 -sha256 -nodes -keyout sashin-ssl.key \
 -x509 -days 90 -subj "/C=KR/ST=Seoul/L=podong_podong_Gaepo-dong/O=42seoul/OU=Cluster/CN=localhost" -out sashin-ssl.crt
