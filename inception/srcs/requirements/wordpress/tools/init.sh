ec17c170428e583cd8ea9caf9f87b3162c608480
Sangbeom Shin <sashin@c3r2s7.42seoul.kr>
2022-07-18 21:00:41 +0900

    update env(user stat)

--- inception/srcs/requirements/wordpress/tools/init.sh
+++ inception/srcs/requirements/wordpress/tools/init.sh
@@ -8,8 +8,8 @@ if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
 
 	cd /var/www/html/wordpress
 
-	wp core install --allow-root --url="$DOMAIN_NAME" --title="${WORDPRESS_NAME}" --admin_user="${WORDPRESS_ROOT_LOGIN}" --admin_password="${WORDPRESS_ROOT_PASSWORD}" --admin_email="${WORDPRESS_ROOT_EMAIL}" --skip-email
-	wp user create --allow-root "${MYSQL_USER}" "${WORDPRESS_USER_EMAIL}" --user_pass="${MYSQL_PASSWORD}" --role=author
+	wp core install --allow-root --url="${DOMAIN_NAME}" --title="${WORDPRESS_NAME}" --admin_user="${WORDPRESS_ROOT_LOGIN}" --admin_password="${WORDPRESS_ROOT_PASSWORD}" --admin_email="${WORDPRESS_ROOT_EMAIL}" --skip-email
+	wp user create --allow-root "${WORDPRESS_USER_LOGIN}" "${WORDPRESS_USER_EMAIL}" --user_pass="${WORDPRESS_USER_PASSWORD}" --role=author
 fi
 
 exec php-fpm7.3 -F
